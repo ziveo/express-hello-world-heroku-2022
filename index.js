6e746cdb168b09971cb38ce8be81861b9669147d
ziveo <bziveo@gmail.com>
2022-02-16 22:09:38 -0500

    Updating PORT config logic v2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.cookie('cookieName', 'cookieValue', {
@@ -14,6 +14,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Example app listening on port http://127.0.0.1:${PORT}`);
 });
